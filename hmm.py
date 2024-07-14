e76335ed9ce696a82cf7fe33298aedb69e519ab3
AkimFA <104549356+AkimFA@users.noreply.github.com>
2024-07-14 13:28:05 +0800

    Training data set

--- hmm.py
+++ hmm.py
@@ -11,16 +11,16 @@ n_observations = len(observations)
 state_probability = np.array([0.9,0.1])
 
 transition_probability = np.array([
-    [32/35,3/35],
-    [0.4,0.6]
+    [0.791,0.209],
+    [0.933,0.067]
 ])
 
 emission_probability= np.array([
-    [0,0,1/6,1/4,1/3,1/4],
-    [0.5,0.5,0,0,0,0]
+    [0.002,0.221,0.143,0.172,0.225,0.237],
+    [0.944,0.056,0,0,0,0]
 ])
 
-model = hmm.CategoricalHMM(n_components=n_states, n_features=n_observations)
+model = hmm.MultinomialHMM(n_components=n_states)
 model.startprob_ = state_probability
 model.transmat_ = transition_probability
 model.emissionprob_ = emission_probability
@@ -29,6 +29,7 @@ observations_sequence = np.array([0,1,4,3,2,0,1,0,1,2,3,3,4,4,3,4,5,5,3,0,1,4,4,
 observations_sequence
 model.fit(observations_sequence)
 model.predict(observations_sequence)
+print(model.score(observations_sequence))
 
 print(model.transmat_)
 print(model.emissionprob_)
