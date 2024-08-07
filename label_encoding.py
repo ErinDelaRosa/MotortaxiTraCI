2ddfa767edefe734acbac3865b2662680e2b6adc
AkimFA <104549356+AkimFA@users.noreply.github.com>
2024-08-07 22:23:13 +0800

    Small change to label_encoding

--- label_encoding.py
+++ label_encoding.py
@@ -5,6 +5,3 @@ from sklearn.preprocessing import LabelEncoder
 df = pd.read_csv("sequence.csv") # CSV file contatining sequence of observations
 sequences = df['Observations'].apply(lambda x: ['STOP', 'GO', 'LEFT SEEP', 'RIGHT SEEP', '1-LEFT', '1-RIGHT'].index(x))
 final_sequence = sequences.to_numpy() 
-
-v_sequences = df["Validation Observations"].dropna().apply(lambda x: ['STOP', 'GO', 'LEFT SEEP', 'RIGHT SEEP', '1-LEFT', '1-RIGHT'].index(x))
-final_v_sequences = v_sequences.to_numpy()
